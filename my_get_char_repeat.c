423c6dde3a0b470c6f1d7833a68a96f26bc02d76
Bertrand <bertrandkalife0907@gmail.com>
2023-07-12 14:49:15 +0200

    commit 2 char repeat avec NULL

--- my_get_char_repeat.c
+++ my_get_char_repeat.c
@@ -1,11 +1,19 @@
 
+#include <stdio.h>
+
 void print_number();
 
 int my_get_char_repeat(char to_find, char* str){
-    int i=0;
-    for(int x=0; str[x]; x++){
-
-        if (str[x]== to_find)i++;
+   
+    if(str == NULL){
+        print_number(0);
     }
-    print_number(i);
+    else{
+        int i=0;
+        for(int x=0; str[x]; x++){
+
+                if (str[x]== to_find)i++;
+            }
+       print_number(i);
+    }
 }
