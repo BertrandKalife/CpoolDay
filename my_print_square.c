ccc5a320cc78f831077e57cdc7636b9ca7d728f1
Bertrand <bertrandkalife0907@gmail.com>
2023-07-12 16:19:19 +0200

    commit print square 1

---
+++ my_print_square.c
@@ -0,0 +1,14 @@
+
+void print_char();
+
+char* my_print_square(int i, char c){
+
+    for(int x=0; x<i;x++){
+        for(int x=0; x<i;x++){
+
+            print_char(c);
+        }
+        print_char('\n');
+    }
+
+}
