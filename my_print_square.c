ad0366d3ce20e1c9b2fd319253189ca44ed4b610
Bertrand <bertrandkalife0907@gmail.com>
2023-07-12 16:22:37 +0200

    commit print square 2

--- my_print_square.c
+++ my_print_square.c
@@ -2,13 +2,13 @@
 void print_char();
 
 char* my_print_square(int i, char c){
-
-    for(int x=0; x<i;x++){
+    if(i>0){
         for(int x=0; x<i;x++){
+            for(int x=0; x<i;x++){
 
-            print_char(c);
+                print_char(c);
+            }
+            print_char('\n');
         }
-        print_char('\n');
     }
-
 }
