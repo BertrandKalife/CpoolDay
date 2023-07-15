3f35b4fa193dd7489c05c198896e509438ac638f
Bertrand <bertrandkalife0907@gmail.com>
2023-07-15 14:27:14 +0200

    my_average commit

--- my_average.c
+++ my_average.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+
+
+float my_average (int* lst, int taille){
+
+    float i=0;
+
+    for(int x=0; x<taille; x++){
+
+       i = i + lst[x];
+    }
+
+    float a= i/taille;
+    
+    return a;
+}
+
+
+
 int main(void)
 {
     int list[5] = {10, 17, 6, 14, 20};
